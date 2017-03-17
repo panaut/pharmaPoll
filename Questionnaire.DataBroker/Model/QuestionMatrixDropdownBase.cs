40313f61ef4254f7e010a9049daf11a0dbdc4c7f
panaut <ivan.cojbasic@gmail.com>
2017-03-17 17:21:42 +0100

    ~Addendum to last commit...

--- Questionnaire.DataBroker/Model/QuestionMatrixDropdownBase.cs
+++ Questionnaire.DataBroker/Model/QuestionMatrixDropdownBase.cs
@@ -9,9 +9,18 @@ namespace Questionnaire.DataBroker.Model
 {
     public class QuestionMatrixDropdownBase : QuestionBaseSelect
     {
-        public string columnMinWidth { get; set; } = "";
+        public string columnMinWidth { get; set; }
+
+        public bool ShouldSerializecolumnMinWidth()
+        {
+            return !string.IsNullOrEmpty(this.columnMinWidth);
+        }
 
         public bool horizontalScroll { get; set; } = false;
+        public bool ShouldSerializehorizontalScroll()
+        {
+            return this.horizontalScroll;
+        }
 
         [JsonProperty(ItemTypeNameHandling = TypeNameHandling.None)]
         public IEnumerable<MatrixDropdownColumn> columns { get; set; }
