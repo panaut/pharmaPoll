f05fc625d552d5d4e2cef3deaa11199c2681b147
panaut <ivan.cojbasic@gmail.com>
2017-04-24 14:56:28 +0200

    Changed Namespace for Comment question

--- Questionnaire.Data/Model/QuestionDefinition/Comment.cs
+++ Questionnaire.Data/Model/QuestionDefinition/Comment.cs
@@ -3,7 +3,7 @@ using Questionnaire.Serialization.Attributes;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Questionnaire.Serialization.Model
+namespace Questionnaire.Serialization.Model.QuestionDefinition
 {
     [SurveyType("comment")]
     [Table("QDefBases_Comment")]
