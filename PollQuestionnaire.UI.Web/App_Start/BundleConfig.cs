4b799353858e9efa7304f2b649749fd525333b78
panaut <ivan.cojbasic@gmail.com>
2017-06-04 15:12:46 +0200

    Added language selection, and redesigned PatQueSurvey Page

--- PollQuestionnaire.UI.Web/App_Start/BundleConfig.cs
+++ PollQuestionnaire.UI.Web/App_Start/BundleConfig.cs
@@ -32,6 +32,7 @@ namespace PollQuestionnaire.UI.Web
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap-select-min.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
